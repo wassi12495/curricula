6048446a36839a68f50421cfadcbbfb7dfab3ccd
github username <github email address>
2017-12-05 13:58:39 -0500

    added a show and index page from books as well as the beginnings of a navbar

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,11 @@ class BooksController < ApplicationController
 
 
   def index
-
+    @books = Book.all
   end
 
   def show
-
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -15,10 +15,25 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.valid?
+      @book.save
+      redirect_to book_path(@book)
+    else
+      redirect_to new_book_path
+    end
 
   end
 
   def edit
+
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :year, :publisher, :description)
 
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,8 @@ class Book < ApplicationRecord
   has_many :book_subjects
   has_many :curriculums, through: :curriculum_books
   has_many :subjects, through: :book_subjects
+
+
+  validates :title, presence: true
+  
 end
