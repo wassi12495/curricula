c3df4a8bb321983fa23d5a15005926c4f2be88ed
github username <github email address>
2017-12-06 08:41:22 -0500

    made it look cool

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,24 @@
 class UsersController < ApplicationController
 
-  skip_before_action :authorized, only: [:new, :create]
-   def create
-     @user = User.new(user_params)
-     if @user.valid?
-       @user.save
-       session[:user_id] = @user.id
-       redirect_to books_path
-     else
-       render 'new'
-     end
-   end
+  # skip_before_action :authorized, only: [:new, :create, :index]
+  #  def create
+  #    @user = User.new(user_params)
+  #    if @user.valid?
+  #      @user.save
+  #      session[:user_id] = @user.id
+  #      redirect_to books_path
+  #    else
+  #      render 'new'
+  #    end
+  #  end
 
   def new
      @user = User.new
-   end
+  end
+
+  def index
+    @users = User.all
+  end
 
 
   def show
@@ -25,7 +29,7 @@ class UsersController < ApplicationController
 
 
   def books
-   end
+  end
 
 
 
