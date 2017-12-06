d969db835ca37f266f3450f5c66641e55fd88c9e
github username <github email address>
2017-12-06 11:09:33 -0500

    editted curriculum create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
 
   private
      def user_params
-       params.require(:user).permit(:username, :password, :password_confirmation)
+       params.require(:user).permit(:username, :password, :password_confirmation, :first_name, :last_name)
      end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :curriculums
+
+  validates :first_name, presence: true
+
+  def name
+    "#{self.first_name}" + "#{self.last_name}"
+  end
 end
