c9163793bd108c2b2a41478bc22ac3ff62072733
github username <github email address>
2017-12-07 17:53:38 -0500

    adding img_url to subject

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207201641) do
+ActiveRecord::Schema.define(version: 20171207224740) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20171207201641) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "img_url"
   end
 
   create_table "users", force: :cascade do |t|
