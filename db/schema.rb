c1b7bcd89dc207a4961a1d117c8c67b7452433c6
github username <github email address>
2017-12-07 11:11:27 -0500

    added img_url to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206210633) do
+ActiveRecord::Schema.define(version: 20171207161032) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20171206210633) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "biography"
+    t.string "img_url"
   end
 
 end
